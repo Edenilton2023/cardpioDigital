4fee9f4feda76437a118c9b5c4371c56e575f180
Edenilton <142240436+Edenilton2023@users.noreply.github.com>
2024-07-03 01:57:44 -0300

    imgen manipuçlaçao
    avanço nao img

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@
 
 
 
+
 const botaoLi = document.querySelectorAll('.topo li');
 const secaoId = document.querySelectorAll('section');
 
@@ -25,36 +26,51 @@ botaoLi.forEach((botao) => {
   botao.addEventListener('click', (event) => {
     secaoId.forEach((item) => {
       item.classList.remove('aparecer'); // Esconde todas as seções
-
+     
       const botaoAtua = event.target;
       if (botaoAtua.id === item.id) {
         item.classList.add('aparecer'); // Mostra a seção correspondente
+      }
+    });   
+  });
+});
+
+ 
+ 
+
+const $pratoDetalhes = document.querySelector('.prato-detalhes');
+const $fotoPequena = document.querySelectorAll(".fotoPequena")
+ const $fotoGrande = document.querySelectorAll('.fotoGrand');
+
+$fotoPequena.forEach((foto)=>{
+  foto.addEventListener('click',()=>{
+    console.log(foto.id);
+$fotoGrande.forEach((fotog)=>{
+fotog.classList.add('esconde')
+foto.classList.add('mostra');
+
+
+  if (fotog.id === foto.id){
+    foto.classList.add('esconde')
+    fotog.classList.remove('esconde')
+  }
+})
+
+
+  })
+})
+$fotoGrande.forEach((foto) => {
+  foto.addEventListener('click', () => {
+    console.log(foto.id);
+    $fotoPequena.forEach((fotog) => {
+      if (fotog.id === foto.id) {
+        foto.classList.add('esconde');
+        fotog.classList.remove('esconde');
       }
     });
   });
 });
 
-
- const trocarFoto =(itemA ,itemB)=>{
-  if (itemA.classList.contains('ativa')){
-    itemA.classList.add('esconde')&& itemB.classList.remove("esconde")
-
-  }
- }
-
-const $pratoDetalhes = document.querySelector('.prato-detalhes');
- const $combo = document.querySelector('.combos')
- const $fotoGrande = document.querySelector('.flex-container');
-
-$pratoDetalhes.addEventListener('click',()=>{
- $pratoDetalhes.classList.add('esconde'),$fotoGrande.classList.remove('esconde')
-}
-)
-$fotoGrande.addEventListener('click', () => {
-$fotoGrande.classList.add('esconde'),
-    $pratoDetalhes.classList.remove('esconde');
-});
-//  a iadeia e fazer esta funçao trocar baseada no id de cad alelemeto 
 
 
  
