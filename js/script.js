416233ea9f4fc010dacc990d14d132814192301a
Edenilton <142240436+Edenilton2023@users.noreply.github.com>
2024-07-27 02:31:15 -0300

    moviemeto da foto

--- js/script.js
+++ js/script.js
@@ -4,73 +4,56 @@
 
 
 
+document.querySelectorAll('.meuBotao')./*pegando todos os botoes*/forEach((button) => {
+  button.addEventListener('click', function () {
+    const targetId = this.getAttribute('data-target');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const botaoLi = document.querySelectorAll('.topo li');
-const secaoId = document.querySelectorAll('section');
-
-botaoLi.forEach((botao) => {
-  botao.addEventListener('click', (event) => {
-    secaoId.forEach((item) => {
-      item.classList.remove('aparecer'); // Esconde todas as seções
-     
-      const botaoAtua = event.target;
-      if (botaoAtua.id === item.id) {
-        item.classList.add('aparecer'); // Mostra a seção correspondente
-      }
-    });   
-  });
-});
-
- 
- 
-
-const $pratoDetalhes = document.querySelector('.prato-detalhes');
-const $fotoPequena = document.querySelectorAll(".fotoPequena")
- const $fotoGrande = document.querySelectorAll('.fotoGrand');
-
-$fotoPequena.forEach((foto)=>{
-  foto.addEventListener('click',()=>{
-    console.log(foto.id);
-$fotoGrande.forEach((fotog)=>{
-fotog.classList.add('esconde')
-foto.classList.add('mostra');
-
-
-  if (fotog.id === foto.id){
-    foto.classList.add('esconde')
-    fotog.classList.remove('esconde')
-  }
-})
-
-
-  })
-})
-$fotoGrande.forEach((foto) => {
-  foto.addEventListener('click', () => {
-    console.log(foto.id);
-    $fotoPequena.forEach((fotog) => {
-      if (fotog.id === foto.id) {
-        foto.classList.add('esconde');
-        fotog.classList.remove('esconde');
-      }
+    document.querySelectorAll('.secao').forEach((secao) => {
+      secao.classList.remove('ativa');
     });
+    document.getElementById(targetId).classList.add('ativa');
   });
 });
 
 
+// const troca = (item) =>{
+//     item.classList.toggle('secao')
+// }
 
- 
+// const prato = document.querySelector('.pai')
+// const $pratos = Array.from(prato)
+
+// console.log(typeof prato);
+
+// prato.addEventListener('click',()=>{
+//   prato.children[1].classList.toggle('secao') 
+//   prato.children[0].classList.toggle('secao'); 
+
+
+// })
+
+const pratos = Array.from(document.querySelectorAll('.pai')) 
+
+
+
+
+
+
+pratos.map((item)=>{
+item.addEventListener('click',()=>{
+if (item.children.length >= 2) {
+// Alterna a classe 'secao' para os filhos do elemento
+
+item.children[1].classList.toggle('secao'); 
+item.children[0].classList.toggle('secao');
+} 
+})
+})
+   
+        
+       
+      
+
+
+
+
